2bb26d37eb5c42c884a40e2ed12b465f7c30f5a4
cyilin <cyilin@users.noreply.github.com>
2017-12-11 22:20:13 +0800

    add support for infernalmobs 5.1.25

--- src/main/java/cat/nyaa/autobloodmoon/api/InfernalMobsAPI.java
+++ src/main/java/cat/nyaa/autobloodmoon/api/InfernalMobsAPI.java
@@ -6,16 +6,17 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.Plugin;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public class InfernalMobsAPI {
     private static Method spawnMobMethod;
     private static Method isInfernalMobMethod;
+    private static Enum<?>[] enumAbilities = null;
 
     public static void load(Plugin InfernalMobs) {
         try {
@@ -28,18 +29,42 @@ public class InfernalMobsAPI {
                     UUID.class
             );
         } catch (ReflectiveOperationException e) {
-            e.printStackTrace();
+            // IM v5.1.25
+            try {
+                spawnMobMethod = Class.forName("com.jacob_vejvoda.infernal_mobs.api.InfernalMobsAPI").getMethod(
+                        "spawnInfernalMob",
+                        EntityType.class, Location.class, Collection.class
+                );
+                isInfernalMobMethod = Class.forName("com.jacob_vejvoda.infernal_mobs.api.InfernalMobsAPI").getMethod(
+                        "asInfernalMob",
+                        UUID.class
+                );
+                enumAbilities = (Enum<?>[]) Class.forName("com.jacob_vejvoda.infernal_mobs.ability.EnumAbilities").getEnumConstants();
+            } catch (NoSuchMethodException | ClassNotFoundException e1) {
+                e1.printStackTrace();
+            }
         }
     }
 
     @SuppressWarnings("deprecation")
     public static boolean spawnMob(String mobName, ArrayList<String> abilityList, Location loc) {
         try {
+            List<Enum<?>> list = new ArrayList<>();
+            if (enumAbilities != null) {
+                for (String abilityName : abilityList) {
+                    for (Enum<?> enumAbility : enumAbilities) {
+                        if (abilityName.toUpperCase().equals(enumAbility.name())) {
+                            list.add(enumAbility);
+                            break;
+                        }
+                    }
+                }
+            }
             EntityType type = EntityType.fromName(mobName);
             if (type == null) {
                 return false;
             }
-            Object mob = spawnMobMethod.invoke(null, type, loc, abilityList);
+            Object mob = spawnMobMethod.invoke(null, type, loc, enumAbilities == null ? abilityList : list);
             return mob != null;
         } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
